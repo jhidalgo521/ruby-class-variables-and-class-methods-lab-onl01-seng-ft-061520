f3a33e807bcaac0d498f6e0cdac1e507a01fb658
Jose Hidago <jhidalgo521@gmail.com>
2020-06-26 18:50:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,7 +31,10 @@ class Song
   def self.genre_count
     genre_count = {}
     @@genres.each do |genre| 
-    if genre_count[genre]
+    if genre_count[genre] 
+       genre_count[genre]+= 1 
+    else 
+       
   end
   
   
