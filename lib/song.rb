ed7e4fcbcfd9eda14a27594866dd13a87c15b26c
Jose Hidago <jhidalgo521@gmail.com>
2020-06-26 18:39:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,6 +5,7 @@ class Song
   
   @@count = 0 
   @@artists = []
+  @@genres = []
   
   def initialize(name, artist, genre)
     @name = name 
@@ -12,6 +13,7 @@ class Song
     @genre = genre 
     @@count += 1
     @@artists << artist 
+    @@genres << genre
   end 
   
 end 
