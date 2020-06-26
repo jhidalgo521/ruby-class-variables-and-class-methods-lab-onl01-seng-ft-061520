862dde3b308c4fd6d47f31e4c7a2a58381abfac8
Jose Hidago <jhidalgo521@gmail.com>
2020-06-26 18:35:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,12 +4,13 @@ class Song
   attr_accessor :name, :artist, :genre 
   
   @@count = 0 
-  
+  @@arist = []
   def initialize(name, artist, genre)
     @name = name 
     @artist = artist
     @genre = genre 
     @@count += 1
+    @@artist += artist 
   end 
   
 end 
